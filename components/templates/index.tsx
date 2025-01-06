92f1cf8d43d2b5f3a49b22e2716b16c952b2a1c1
luthfi sugara <sugaraluthfi@gmail.com>
2025-01-06 19:47:21 +0700

    fix(ui): not found page

--- components/templates/index.tsx
+++ components/templates/index.tsx
@@ -2,10 +2,12 @@ import Header from "./Header";
 import Layout from "./Layout";
 import Footer from "./Footer";
 import DocumentationCaard from "./DocumentationCard";
+import NotFoundPage from "./NotFoundPage";
 
 export {
     Layout,
     Header,
     Footer,
     DocumentationCaard,
+    NotFoundPage,
 }
