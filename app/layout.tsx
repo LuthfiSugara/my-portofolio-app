297c2f01505dcdc77b9787f56151535abc384fdd
luthfi sugara <sugaraluthfi@gmail.com>
2025-01-09 22:33:01 +0700

    feat(head): change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Layout } from "@/components/templates";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
@@ -15,10 +16,10 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Luthfi Sugara",
-  description: "Full-Stack Software Developer with 6 years Structuring, Develop and Implementing interactive applications.",
-};
+// export const metadata: Metadata = {
+//   title: "Luthfi Sugara",
+//   description: "Full-Stack Software Developer with 6 years Structuring, Develop and Implementing interactive applications.",
+// };
 
 export default function RootLayout({
   children,
@@ -27,6 +28,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Luthfi Sugara</title>
+        <meta name="description" content="Full-Stack Software Developer with 6 years Structuring, Develop and Implementing interactive applications." />
+        <meta property="og:title" content="Luthfi Sugara" />
+        <meta property="og:description" content="Full-Stack Software Developer with 6 years Structuring, Develop and Implementing interactive applications." />
+        <meta property="og:image" content="https://sugara.online/images/Logo.png" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://sugara.online" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="L:Luthfi Sugara" />
+        <meta name="twitter:description" content="Full-Stack Software Developer with 6 years Structuring, Develop and Implementing interactive applications." />
+        <meta name="twitter:image" content="https://sugara.online/images/Logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased hide-scrollbar`}
       >
